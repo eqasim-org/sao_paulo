4e2b2d5245c3c0ecd3347643cfe50e2070729b87
balacmi <milos.balac@ivt.baug.ethz.ch>
2020-05-30 20:54:59 +0200

    Update zones.py
    using raw data and dissolving in the pipeline

--- data/spatial/zones.py
+++ data/spatial/zones.py
@@ -7,16 +7,13 @@ def configure(context):
     context.config("data_path")
 
 def execute(context):
-    df_zones_hts = gpd.read_file("%s/Spatial/Zonas_2017_region.shp" % context.config("data_path"))
-    df_zones_hts.crs = {"init" : "EPSG:29183"}    
-    df_zones_hts = df_zones_hts[['NumeroZona', 'geometry']]
-    df_zones_hts.columns = ["zone_id", "geometry"]
     
-    df_zones_census = gpd.read_file("%s/Spatial/SC2010_RMSP_CEM_V3_merged.shp" % context.config("data_path"))
+    df_zones_census = gpd.read_file("%s/Spatial/SC2010_RMSP_CEM_V3.shp" % context.config("data_path"))
     df_zones_census.crs = {"init" : "EPSG:4326"}
-    df_zones_census = df_zones_census[['geometry', 'AP_2010_CH']]
-    df_zones_census.columns = [ "geometry", "zone_id"]
-    df_zones_census = df_zones_census.to_crs({"init" : "EPSG:29183"})
-    df_zones_census["zone_id"] = df_zones_census["zone_id"].astype(np.int)
+    df_zones_census_dissolved = df_zones_census.dissolve(by='AP_2010') 
+    df_zones_census_dissolved = df_zones_census_dissolved[['geometry', 'AP_2010_CH']]
+    df_zones_census_dissolved.columns = [ "geometry", "zone_id"]
+    df_zones_census_dissolved = df_zones_census_dissolved.to_crs({"init" : "EPSG:29183"})
+    df_zones_census_dissolved["zone_id"] = df_zones_census_dissolved["zone_id"].astype(np.int)
 
-    return df_zones_census,df_zones_hts
+    return df_zones_census_dissolved
