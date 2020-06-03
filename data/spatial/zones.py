bf24213a1d1544c2429d4dde74717b5d10aec45c
Aurore Sallard <aurore.sallard@ivt.baug.ethz.ch>
2020-06-03 18:14:57 +0200

    Fixed minor naming issues

--- data/spatial/zones.py
+++ data/spatial/zones.py
@@ -10,9 +10,11 @@ def execute(context):
     
     df_zones_census = gpd.read_file("%s/Spatial/SC2010_RMSP_CEM_V3.shp" % context.config("data_path"))
     df_zones_census.crs = {"init" : "EPSG:4326"}
-    df_zones_census_dissolved = df_zones_census.dissolve(by='AP_2010') 
+    df_zones_census_dissolved = df_zones_census#.dissolve(by='AP_2010_CH') 
     df_zones_census_dissolved = df_zones_census_dissolved[['geometry', 'AP_2010_CH']]
+    print(df_zones_census_dissolved.head())
     df_zones_census_dissolved.columns = [ "geometry", "zone_id"]
+    print(df_zones_census_dissolved.head())
     df_zones_census_dissolved = df_zones_census_dissolved.to_crs({"init" : "EPSG:29183"})
     df_zones_census_dissolved["zone_id"] = df_zones_census_dissolved["zone_id"].astype(np.int)
 
