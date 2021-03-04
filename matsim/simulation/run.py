48b82a341c25a475224f002bc13da45f7a777c97
balacm <milos.balac@ivt.baug.ethz.ch>
2021-03-04 22:59:04 +0100

    make it run for 90 iterations; and 10% replanning

--- matsim/simulation/run.py
+++ matsim/simulation/run.py
@@ -18,8 +18,9 @@ def execute(context):
     # Run routing
     eqasim.run(context, "org.eqasim.sao_paulo.RunSimulation", [
         "--config-path", config_path,
-        "--config:controler.lastIteration", str(1),
-        "--config:controler.writeEventsInterval", str(1),
-        "--config:controler.writePlansInterval", str(1),
+        "--config:controler.lastIteration", str(90),
+        "--config:controler.writeEventsInterval", str(10),
+        "--config:controler.writePlansInterval", str(10),
+        "--config:strategy.strategysettings[strategyName=DiscreteModeChoice].weight", str(0.1),
+        "--config:strategy.strategysettings[strategyName=KeepLastSelected].weight", str(0.9),
     ])
-    assert os.path.exists("%s/simulation_output/output_events.xml.gz" % context.path())
